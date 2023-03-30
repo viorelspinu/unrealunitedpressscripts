9e12871cdcd6ae4fec24f4136855e69f16a1cb78
Viorel Spinu <viorel.spinu@cegeka.com>
2023-03-30 13:03:48 +0300

    a

--- add_world_image_9.js
+++ add_world_image_9.js
@@ -13,7 +13,11 @@ function addWorldImage() {
             imgSrc = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiYpJekrlujWnB5emlylBsvgaLRXlN55oMVeNh-1_GzQor6bplIzwkM-B7-u4p5v9EEbmNpua5Eq9rt6zddPaIdlm284ZtMGyG-xS9pSqJ4I9aAg8b5TuRwnnILyd9q0uI4tHEVZ3IPjtf601TpcDzCFsFwYK1UTp6JT_fCrowvGO-aUmRxHwXIfxv1/s16000/aeris4.jpg";
         } else if (labelText.startsWith("Arinthia")) {
             imgSrc = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjyqO2TQqXjQiL_HvWgFdqaQkSfCWbVb1zegauXQAMXxRkiVG5ihQ0hSRGAHAGEKt0Cv_IsOkKf_-sLzQeCeqU2i5Bn32Pq_7iTIFKTm5P9VaatDZKNHDH6BrC4k4Y4GD0dahNRZblPQDtVcc2N5_8ePdH40pdWu0okj-i4C0DV1_skjBW7nLdj9J8T/s16000/arinthia.jpg";
-        } else {
+        } else if (labelText.startsWith("Behemoth")) {
+            imgSrc = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiEnmPri7oNCimGJESO3AuR7clQAjRI65u17TnSqSO5ta0Gme31zNknYeE9YOjzWAjqyw8S0OdA4XpNcmkthsFry8KdYdIOa2WnzQpD72TL3uMQLeGRIJbkbjxzc3IoxCheN5PidLEITystyie5L0YKx8Ykc1KxdBrjaoBomnRL_JIBXYDGyMNNIBJa/s16000/behemoth.jpg"
+        }
+        
+        else {
             imgSrc = "";
         }
 
