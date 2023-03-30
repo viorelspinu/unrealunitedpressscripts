8569c3ab1d893139a580138876c1860334e4039f
Viorel Spinu <viorel.spinu@cegeka.com>
2023-03-30 13:06:20 +0300

    a

--- add_world_image_10.js
+++ add_world_image_10.js
@@ -15,6 +15,8 @@ function addWorldImage() {
             imgSrc = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjyqO2TQqXjQiL_HvWgFdqaQkSfCWbVb1zegauXQAMXxRkiVG5ihQ0hSRGAHAGEKt0Cv_IsOkKf_-sLzQeCeqU2i5Bn32Pq_7iTIFKTm5P9VaatDZKNHDH6BrC4k4Y4GD0dahNRZblPQDtVcc2N5_8ePdH40pdWu0okj-i4C0DV1_skjBW7nLdj9J8T/s16000/arinthia.jpg";
         } else if (labelText.startsWith("Behemoth")) {
             imgSrc = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiEnmPri7oNCimGJESO3AuR7clQAjRI65u17TnSqSO5ta0Gme31zNknYeE9YOjzWAjqyw8S0OdA4XpNcmkthsFry8KdYdIOa2WnzQpD72TL3uMQLeGRIJbkbjxzc3IoxCheN5PidLEITystyie5L0YKx8Ykc1KxdBrjaoBomnRL_JIBXYDGyMNNIBJa/s16000/behemoth.jpg"
+        } else if (labelText.startsWith("Crystalline")){
+            imgSrc = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjqZ1MA-Zti1QW42EjL2gthS-K2GrHbRbY90a9COiOIhPEwlfSrKhj9rhqGYC_7q95OxIZxsCzof0D7ULcP6gtFMnQV1-4l4tn-s0Thns_7ByJBQrB99kWExPYj9FM60tooMAEi_jD-ukCuY_tuPUC0jJu-Y8wKzzPW9sUz9taVJEg6a_ewvdFPSp_j/s16000/crytalline.jpg"
         }
         
         else {
